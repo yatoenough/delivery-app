706e5d92ece83ea3a5eb07614242580d95e8a436
yatoenough <yatoenough@gmail.com>
2024-06-09 19:09:03 +0200

    installed prisma, connected to db, defined user model and migration

---
+++ prisma/migrations/20240609170259_users/migration.sql
@@ -0,0 +1,18 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "avatarPath" TEXT NOT NULL DEFAULT 'uploads/avatars/default-avatar.png',
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_username_key" ON "users"("username");
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
-	await app.listen(3000);
+	const port = process.env.PORT;
+
+	app.enableCors();
+	app.setGlobalPrefix('/api');
+
+	await app.listen(port, () =>
+		new Logger().log(`Application started on port ${port}`, 'NestApplication')
+	);
 }
 bootstrap();
